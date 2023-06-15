94a553b43a8a7dd953a88c9dadc87fc06474768c
Poojakadam23 <poojakiran660@gmail.com>
2023-06-15 10:46:10 +0530

    list using map

--- src/App.js
+++ src/App.js
@@ -4,20 +4,24 @@ function App() {
   return (
     <>
     <h1>Props demo</h1>
-
-    <MessageDemo username="Pinky"/>
-    <MessageDemo username="Pooja" email="P@gmail.com"/>
-    <MessageDemo username="Nikhil"email="N@gmail.com"/>
-    <MessageDemo username="Pradip"email="G@gmail.com"/>
+    <ListDemo />
 
     </>
   );
 }
-function MessageDemo({email,username}){
+function ListDemo(){
+  let data = "Hello Universe";
+  let list = ["delhi", "calcutta", "chennai"];
+
   return(
     <>
-    <h1>Hello {username} {email} </h1>
-    </>
+    
+    <h1>{data}</h1>
+    {list.map((item)=>(<><h1>
+      Hello World
+    </h1></>
+    ))}
+  </>
   );
 }
 export default App;
