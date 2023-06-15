464df3cf64b2c43cb926daef6b9a1d2676c93bd9
Poojakadam23 <poojakiran660@gmail.com>
2023-06-15 14:40:51 +0530

    another-func-props

--- src/App.js
+++ src/App.js
@@ -26,12 +26,23 @@ function ListDemo(){
     <input type="text" id="id1"ref={inputRef}placeholder="Enter user input..."/>
     <input type="button"value="Add New Item" onClick={addItemAction}/>
     
-    
-    {list.map((item)=>(<><h1>
-      {item}
-    </h1></>
-    ))}
-  </>
+   {list.map((item)=>(
+    <div>
+      <h1>{item}</h1>
+      <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore,
+       sit laborum quia non hic doloribus id quisquam consequuntur quis
+      iusto quod, assumenda vitae, molestias ipsa ex labore quaerat
+      ducimus accusamus.
+
+      </p>
+      <div>
+        <input type="button" value="&#128077;"/>
+        <input type="button" value="&#128078;"/>
+      </div>
+    </div>
+   ))}
+   </>
   );
 }
 export default App;
