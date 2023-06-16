c309a0034486c974c6ea8e1f8689a9c94be3d586
Poojakadam23 <poojakiran660@gmail.com>
2023-06-16 14:58:39 +0530

    button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter,
+  Link,
+  Route,
+  Routes,
+  useNavigate,
+} from "react-router-dom";
 import MyTodo from "./pages/MyTodo";
 import MyRegistration from "./pages/MyRegistration";
-import MyNavigationLinks from "./pages/MyNavigationLinks";
+import MyNavigationLink from "./pages/MyNavigationLink";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <MyNavigationLinks />
+        <MyNavigationLink />
 
         <Routes>
           {/** 1 Route means 1 Page */}
@@ -18,9 +24,35 @@ function App() {
           <Route path="*" element={<h1>Page Not Found</h1>} />
         </Routes>
 
-        <MyNavigationLinks />
+        <MyNavigationLinksUsingButton />
       </BrowserRouter>
     </>
+  );
+}
+
+function MyNavigationLinksUsingButton() {
+  const navigate = useNavigate();
+
+  let goToHomeAction = () => {
+    navigate("/home");
+  };
+  let goToTodoAction = () => {
+    navigate("/todo");
+  };
+  let goToRegistrationAction = () => {
+    navigate("/registration");
+  };
+
+  return (
+    <div>
+      <input type="button" value="Home" onClick={goToHomeAction} />
+      <input type="button" value="Todo" onClick={goToTodoAction} />
+      <input
+        type="button"
+        value="Registartion"
+        onClick={goToRegistrationAction}
+      />
+    </div>
   );
 }
 
